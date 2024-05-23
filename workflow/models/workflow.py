25f221508bb517ccff52f834893a54b967a6fdd8
Mbarekwael <wael.mbarek@horizon-tech.tn>
2024-05-23 13:58:20 +0100

    test function

---
+++ workflow/models/workflow.py
@@ -0,0 +1,66 @@
+import requests
+from odoo import api,models, fields
+
+class Workflow(models.Model):
+    _name='workflow'
+    _description='Qualiopi Workflow'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    indicator_number = fields.Integer(string='Indicator Number')
+    sequence = fields.Integer(string='Sequence', related='indicator_number', store=True)
+    summary = fields.Text(string='Summary')
+    partner_id = fields.Many2many('res.users', string='Assignees')
+    verification_status = fields.Selection([
+    ('not_verified', 'Not Verified'),
+    ('verified', 'Verified'),],
+    string='Verification Status')
+    description = fields.Text(string='Description')
+    link = fields.Char(string='Link')
+    
+    def toggle_verification(self):
+        for record in self:
+            record.verification_status = 'verified' if record.verification_status == 'not_verified' else 'not_verified'
+            
+            if record.verification_status == 'verified':
+                message = "The record has been verified."
+            else:
+                message = "The record is not verified."
+            
+            record.message_post(body=message)
+
+
+    def verify_link(self):
+        for record in self:
+            link = record.link
+            try:
+                response = requests.head(link)
+                if response.status_code == 200:
+                    return {
+                        'type': 'ir.actions.client',
+                        'tag': 'display_notification',
+                        'params': {
+                            'title': 'Success!',
+                            'message': 'The link exists.',
+                            'sticky': False,
+                        }
+                    }
+                else:
+                    return {
+                        'type': 'ir.actions.client',
+                        'tag': 'display_notification',
+                        'params': {
+                            'title': 'Error!',
+                            'message': 'The link does not exist.',
+                            'sticky': False,
+                        }
+                    }
+            except requests.ConnectionError:
+                return {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': 'Error!',
+                        'message': 'The link is not valid.',
+                        'sticky': False,
+                    }
+                }
---
+++ workflow/tests/test_workflow.py
@@ -0,0 +1,69 @@
+import logging
+from odoo.tests.common import TransactionCase
+from odoo import fields
+
+_logger = logging.getLogger(__name__)
+
+class TestWorkflow(TransactionCase):
+
+    def setUp(self, *args, **kwargs):
+        super().setUp()
+
+        self.workflow_01_record = self.env['workflow'].create({
+            'indicator_number': 1,
+            'sequence': 1,
+            'summary': 'Test Summary',
+            'partner_id': [(6, 0, [self.env.ref('base.user_admin').id])],
+            'verification_status': 'not_verified',
+            'description': 'Test Description',
+            'link': 'https://www.example.com',
+        })
+
+    def test_01_workflow_values(self):
+        workflow_id = self.workflow_01_record
+
+        self.assertEqual(workflow_id.partner_id.ids, [self.env.ref('base.user_admin').id])
+
+        self.assertRecordValues(workflow_id, [{
+            'indicator_number': 1,
+            'sequence': 1,
+            'summary': 'Test Summary',
+            'verification_status': 'not_verified',
+            'description': 'Test Description',
+            'link': 'https://www.example.com',
+        }])
+
+        _logger.info('TestWorkflow.test_01_workflow_values executed successfully')
+
+    def test_02_toggle_verification(self):
+        workflow_id = self.workflow_01_record
+        
+
+        workflow_id.toggle_verification()
+        self.assertEqual(workflow_id.verification_status, 'verified')
+        self.assertIn("The record has been verified.", workflow_id.message_ids.mapped('body'))
+
+
+        workflow_id.toggle_verification()
+        self.assertEqual(workflow_id.verification_status, 'not_verified')
+        self.assertIn("The record is not verified.", workflow_id.message_ids.mapped('body'))
+
+        _logger.info('TestWorkflow.test_02_toggle_verification executed successfully')
+
+    def test_03_verify_link(self):
+        workflow_id = self.workflow_01_record
+        
+   
+        action = workflow_id.verify_link()
+        self.assertEqual(action['type'], 'ir.actions.client')
+        self.assertEqual(action['tag'], 'display_notification')
+        self.assertEqual(action['params']['title'], 'Success!')
+        self.assertEqual(action['params']['message'], 'The link exists.')
+
+
+        workflow_id.link = 'https://www.invalid-link.com'
+        action = workflow_id.verify_link()
+        self.assertEqual(action['params']['title'], 'Error!')
+        self.assertIn('The link does not exist.', action['params']['message'])
+
+        _logger.info('TestWorkflow.test_03_verify_link executed successfully')
