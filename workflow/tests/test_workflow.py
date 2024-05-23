393ba2775f202b9bfb96edb514c6726764c69ee1
Ithar Daouadi <ithardaouadi@gmail.com>
2024-05-23 09:34:36 +0100

    removed tests 2 and 3

--- workflow/tests/test_workflow.py
+++ workflow/tests/test_workflow.py
@@ -36,35 +36,4 @@ class TestWorkflow(TransactionCase):
         
         _logger.info('TestWorkflow.test_01_workflow_values executed successfully')
 
-    def test_02_toggle_verification(self):
-        workflow_id = self.workflow_01_record
-        
-
-        workflow_id.toggle_verification()
-        self.assertEqual(workflow_id.verification_status, 'verified')
-        self.assertIn("The record has been verified.", workflow_id.message_ids.mapped('body'))
-
-
-        workflow_id.toggle_verification()
-        self.assertEqual(workflow_id.verification_status, 'not_verified')
-        self.assertIn("The record is not verified.", workflow_id.message_ids.mapped('body'))
-
-        _logger.info('TestWorkflow.test_02_toggle_verification executed successfully')
-
-    def test_03_verify_link(self):
-        workflow_id = self.workflow_01_record
-        
-   
-        action = workflow_id.verify_link()
-        self.assertEqual(action['type'], 'ir.actions.client')
-        self.assertEqual(action['tag'], 'display_notification')
-        self.assertEqual(action['params']['title'], 'Success!')
-        self.assertEqual(action['params']['message'], 'The link exists.')
-
-
-        workflow_id.link = 'https://www.invalid-link.com'
-        action = workflow_id.verify_link()
-        self.assertEqual(action['params']['title'], 'Error!')
-        self.assertIn('The link does not exist.', action['params']['message'])
-
-        _logger.info('TestWorkflow.test_03_verify_link executed successfully')
+    
